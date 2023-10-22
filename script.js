3f07e74d602342f66cf59397fee0673f9601be35
User <user@Users-MacBook-Air.local>
2023-10-21 21:38:41 -0400

    added array of objects with the questions for the quiz game, added timer variable and startTest variable

---
+++ script.js
@@ -0,0 +1,42 @@
+let timer = 75;
+let startTest = false;
+let questions = [
+  {
+    questionNumber: 1,
+    question:
+      "Which of the following keyword is used to instantiat a variable in JavaScript?",
+    choices: ["var", "this", "function", "console.log"],
+    correctAnswer: "var",
+  },
+  {
+    questionNumber: 2,
+    question:
+      "Which language is responsible for making the webpage interactive/dynamic?",
+    choices: ["Java", "HTML", "CSS", "JavaScript"],
+    correctAnswer: "JavaScript",
+  },
+  {
+    questionNumber: 3,
+    question: "What does API stand for?",
+    choices: [
+      "Anonymous Prompt Import",
+      "Application Programming Interface",
+      "All People Intergrate",
+      "All Program Included",
+    ],
+    correctAnswer: "Application Programming Interface",
+  },
+  {
+    questionNumber: 4,
+    question:
+      "Which of the following is the correct file extension for a JavaScript file?",
+    choices: [".javascript", ".js", ".jsx", ".script"],
+    correctAnswer: ".js",
+  },
+  {
+    questionNumber: 5,
+    question: "Which tool in JavaScript is used to debug your code",
+    choices: ["console.log", "function()", "Live Server", "Math.floor"],
+    correctAnswer: "console.log",
+  },
+];
